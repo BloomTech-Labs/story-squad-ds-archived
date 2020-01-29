b05d460f5a48cfb82a7321e6ec7eea3ff4613d63
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2020-01-29 12:15:06 -0500

    Begin accepting multiple images

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -11,10 +11,11 @@ def create_app():
     data = json.loads(data)
 
     data = service_account.Credentials.from_service_account_info(data)
-
+    print('outside')
     @app.route('/')
     def root(methods=['POST']):
-        uri = request.args['image_location']
+        uri = request.args['image_location'].split(',')
+        print('test')
 
         def transcribe(uri):
             """Detects document features in the file located in Google Cloud
@@ -23,10 +24,17 @@ def create_app():
             image = vision.types.Image()
             image.source.image_uri = uri
             response = client.document_text_detection(image=image)
-
+            print(response)
             return response.text_annotations[0].description
         
-        response = {'Transcription':  transcribe(uri)}
+        transcripts = []
+        print(uri)
+        for url in uri:
+            print(url)
+            transcripts.append(transcribe(url))
+        full_text =  ' '.join(transcripts)
+        full_text = full_text.replace('\n', ' ')
+        response = {'Transcription': full_text}
         
         return jsonify(response)
         
