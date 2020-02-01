6d9feaa08d6a25719ac76bb18d1a16e19654412a
William Sedlacek <wsedlacekc@gmail.com>
2020-01-31 18:39:39 -0600

    Restructure into command line interface
    NON FUNCTIONAL!
    
    Todo:
    - Verify stdin/stdout functionality
    - Verify JSON Stringify and parsing
    - Remove Flask boiler plate
    - Setup Node.js glue

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,70 +1,69 @@
+from sys import stdin, stdout
+from json import loads, dump
 from decouple import config
-from flask import Flask, jsonify, request
+
 from google.cloud import vision
 from google.cloud.vision import types
-import urllib.request, json
 from google.oauth2 import service_account
+
+import urllib.request
 import base64
 
-def create_app():
-    app = Flask(__name__)
-    api_key = config('GOOGLE_CREDS')
-    api_key = json.loads(api_key)
-    api_key = service_account.Credentials.from_service_account_info(api_key)
-    @app.route('/', methods=['POST']) # just read that this is where the methods list actually goes
-    def root():
-        # Get json objects
-        body = request.get_json()
-        # print(body)
-        raw_images = body['images']
-        length = raw_images['length']
-        images = []
+api_key = config('GOOGLE_CREDS')
+api_key = loads(api_key)
+api_key = service_account.Credentials.from_service_account_info(api_key)
+
+def processImages(body):
+    raw_images = body['images']
+    length = raw_images['length']
+    images = []
+
+    try:
+        for key in range(length):
+            images.append(base64.b64decode(raw_images[str(key)]))
+    except:
+        print("Encoding error")
+    #build list of images (still assuming urls for now)
+    # images = []
+    # for key in body.keys():
+    #     images.append(base64.b64decode(body[key]))
+
+
+    def transcribe(encoded_image):
+        """Detects document features in the file located in Google Cloud
+        Storage."""
+        client = vision.ImageAnnotatorClient(credentials=api_key)
+        image = types.Image(content=encoded_image)
         try:
-            for key in range(length):
-                images.append(base64.b64decode(raw_images[str(key)]))
+            response = client.document_text_detection(image)
         except:
-            print("Encoding error")
-        #build list of images (still assuming urls for now)
-        # images = []
-        # for key in body.keys():
-        #     images.append(base64.b64decode(body[key]))
-        
-        
-        def transcribe(encoded_image):
-            """Detects document features in the file located in Google Cloud
-            Storage."""
-            client = vision.ImageAnnotatorClient(credentials=api_key)
-            image = types.Image(content=encoded_image)
-            try:
-                response = client.document_text_detection(image)
-            except:
-                return "Error in Google API"
-            if response.text_annotations:
-                return response.text_annotations[0].description
-            else: 
-                return "No Text"
-        
-        #Get transcrtipt (currently just returning diagnostis!)
-        transcripts = []
-        for item in images:
-            transcripts.append(transcribe(item).replace('\n', ' '))
-            # transcripts.append((f'testing\n{item}').replace('\n', ' '))
+            return "Error in Google API"
+        if response.text_annotations:
+            return response.text_annotations[0].description
+        else:
+            return "No Text"
 
-        #Build response dict
-        dic = {}
-        dic['length'] = length
+    transcripts = []
+    for item in images:
+        transcripts.append(transcribe(item).replace('\n', ' '))
+        # transcripts.append((f'testing\n{item}').replace('\n', ' '))
 
-        for key in range(length):
-            dic[str(key)] = transcripts[key]
-        
-        metadata = {}
-        metadata['length'] = length
-        
-        for key in range(length):
-            metadata[str(key)] = {"nothing": "nada"}
-        jason = {'images': dic, 'metadata': metadata}
+    #Build response dict
+    dic = {}
+    dic['length'] = length
 
-        return jsonify(jason)
+    for key in range(length):
+        dic[str(key)] = transcripts[key]
 
+    metadata = {}
+    metadata['length'] = length
 
-    return app
+    for key in range(length):
+        metadata[str(key)] = {"nothing": "nada"}
+    return {'images': dic, 'metadata': metadata}
+
+incoming = stdin.readlines()
+body = loads(incoming)
+processed = processImages(body)
+stringified = dump(processed)
+stdout.print(stringified)
