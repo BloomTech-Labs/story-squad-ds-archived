5ceb3799347c408b17bf7ac9f149f4b8dce96e62
William Sedlacek <wsedlacekc@gmail.com>
2020-02-03 11:46:27 -0600

    Comments and Cleanup
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- integration/src/utils.ts
+++ integration/src/utils.ts
@@ -1,5 +1,10 @@
 import { Transcription } from './models';
 
+/**
+ * @description Attempts to parse a data string into JSON, if it fails it returns the string instead
+ * @param {*} data
+ * @returns {data is Transcription} the parsed JSON or the input string
+ */
 export const attemptJSONParse = (data: string) => {
   try {
     return JSON.parse(data);
@@ -8,5 +13,9 @@ export const attemptJSONParse = (data: string) => {
   }
 };
 
-export const onlyTranscription = (data: any): data is Transcription =>
-  data?.images && data.metadata;
+/**
+ * @description Returns true if the data matches the transcription data shape
+ * @param {*} data
+ * @returns {data is Transcription} if the data is in the transcription data shape
+ */
+export const onlyTranscription = (data: any): data is Transcription => data.images && data.metadata;
--- src/transcription.py
+++ src/transcription.py
@@ -16,6 +16,7 @@ client = vision.ImageAnnotatorClient(credentials=api_key)
 
 
 def transcribe(uri):
+    # Parses a URI and gets the encoded data string out
     data = DataURI(uri).data
     image = types.Image(content=data)
     response = client.document_text_detection(image)
@@ -26,22 +27,41 @@ def transcribe(uri):
         return "No Text"
 
 
+# To be replaced with a function that gets the metadata out of the function
 def nothing(transcript):
     return {"nada": "nothing"}
 
 
+# Input list of URIs
 def process_images(uris):
+    # Map functions are used to take iterable data structures like list, maps, etc. and make a map
+    # that consist of the data of the original data structure after being run through a function.
+    # Outputs a map (list like data structure) of all the uris after being transcribed
     transcripts = map(transcribe, uris)
+    # Outputs a map (list like data structure) of all the transcriptions after having the meta data parsed
     metadata = map(nothing, transcripts)
     return {'images': list(transcripts), 'metadata': list(metadata)}
 
 
+# Input: JSON String in the transcribable data structure
+# Output: JSON String of the data being processed into transcripts and metadata
 def main(transcribable):
     json = loads(transcribable)
     transcriptions = process_images(json['images'])
     return dumps(transcriptions)
 
 
+# Reads in from stdin the entire input as a string
 data = stdin.read()
+
+# Runs the main function using the input string and saving the output string
 output = main(data)
+
+# Prints the output string to stdout
 stdout.write(output)
+
+
+# Can be tested with this command
+# pipenv run python3 src/transcription.py < integration/src/test.json
+
+# Notice how the `test.json` is piped into stdin with the `<`
