9d02c09663b59c1dd15b3fa6f8c00b3ea6952989
William Sedlacek <wsedlacekc@gmail.com>
2020-02-03 12:25:34 -0600

    Better Type Safty
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- integration/src/utils.ts
+++ integration/src/utils.ts
@@ -2,10 +2,10 @@ import { Transcription } from './models';
 
 /**
  * @description Attempts to parse a data string into JSON, if it fails it returns the string instead
- * @param {*} data
- * @returns {data is Transcription} the parsed JSON or the input string
+ * @param {string} data a string that may or may not be JSON data
+ * @returns {Object | string} the parsed JSON or the input string
  */
-export const attemptJSONParse = (data: string) => {
+export const attemptJSONParse = (data: string): Object | string => {
   try {
     return JSON.parse(data);
   } catch {
@@ -14,8 +14,9 @@ export const attemptJSONParse = (data: string) => {
 };
 
 /**
- * @description Returns true if the data matches the transcription data shape
- * @param {*} data
- * @returns {data is Transcription} if the data is in the transcription data shape
+ * @description Returns true if the data matches the Transcription data shape
+ * @param {Object | string} data a string or a parsed JSON that may or may not be in the Transcription data shape
+ * @returns {data is Transcription} if the data is in the Transcription data shape
  */
-export const onlyTranscription = (data: any): data is Transcription => data.images && data.metadata;
+export const onlyTranscription = (data: Object | string): data is Transcription =>
+  data['images'] && data['metadata'];
