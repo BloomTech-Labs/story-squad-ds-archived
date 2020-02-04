83f095d3027a71ff85f99da1d2f9e305ff9984eb
William Sedlacek <wsedlacekc@gmail.com>
2020-02-04 15:24:53 -0600

    More detailed comment
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/scripting.ts
+++ src/scripting.ts
@@ -3,16 +3,40 @@ import { PythonShell } from 'python-shell';
 import { Scripts } from './models';
 
 /**
- * @description
- * @param {keyof Scripts} script The relative path from root to the script to run
- * @param {Scripts[typeof script]['input']} data input data to the script matching the dataType defined in the
- * `Scripts` interface
- * @param {(out: string[]) => Scripts[typeof script]['output']} findResults a callback function that will go
- * through the array of all outputs of the stdout outputs of the script and find the result value.
+ * @description This function is designed to be setup to run Python Scripts from
+ * Node, it uses the `Scripts` model to determine which scripts are valid and what
+ * their inputs and outputs are.
+ *
+ * To use this function first make sure the script you which to use is detailed in
+ * the `Scripts` model with the path and input and output paths, and that the python
+ * script exist in relative path to the root dir, then call this function and provide
+ * the params of the `scriptPath`, `inputData` and `findResult()`. The Python script
+ * will then be setup in an async child process and the result will be returned in a
+ * promise which you can use `await` or `.then()` to work with.
+ *
+ * Note: Be careful with your input and output data types! These are only to help with VS Tooling
+ * and are not checked at run time, so test your code and make sure that the Python script
+ * takes in teh data you are expecting.
+ *
+ * @example
+ * ```ts
+ * function transcribe(data: Transcribable) {
+ *   return runScript('./ds/transcription.py', data, (out) =>
+ *     out.map(attemptJSONParse).find(onlyTranscription)
+ *   );
+ * }
+ * ```
+ *
+ * @param {keyof Scripts} scriptPath The relative path from root to the script to run
+ * @param {Scripts[typeof script]['input']} inputData input data to the script matching
+ * the dataType defined in the `Scripts` interface
+ * @param {(out: string[]) => Scripts[typeof script]['output']} findResults a callback
+ * function that will go through the array of all outputs of the stdout outputs of the
+ * script and find the result value.
  *
  * Note: Some Scripts may stdout when printing out status messages
- * @returns {Promise<ReturnType<typeof findResults>>} A promise of the resulting data after being found with
- * the `findResult()` callback
+ * @returns {Promise<ReturnType<typeof findResults>>} A promise of the resulting data
+ * after being found with the `findResult()` callback
  */
 export const runScript = (
   script: keyof Scripts,
