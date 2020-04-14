59cf33fa01e6a77cea62cd5d1dfa199a2991caee
Andrew Maddocks <andrewmadd@icloud.com>
2020-04-14 10:54:17 -0600

    fixed paths

--- integration/src/example.ts
+++ integration/src/example.ts
@@ -1,21 +1,19 @@
-import { Transcribable } from './models';
-import { runScript } from './scripting';
-import { attemptJSONParse, onlyTranscription } from './utils';
+import { Transcribable } from "./models";
+import { runScript } from "./scripting";
+import { attemptJSONParse, onlyTranscription } from "./utils";
 
 // Wrapper function that runs a specific script
 // Parameters<typeof runScript>[1] is used to specify the second parameter type of `runScript`
 function transcribe(data: Transcribable) {
   return runScript(
-    '../src/transcription.py', // Specifies the script to use, the path is relative to the directory the application is started from
+    "../dotPy/transcription.py", // Specifies the script to use, the path is relative to the directory the application is started from
     data, // The data to pass into stdin of the script
     (out) => out.map(attemptJSONParse).find(onlyTranscription) // A function to take the stdout of the script and find the result
   );
 }
 
 // Loads the test.json file
-const data = require('./test.json');
+const data = require("./transcription_test.json");
 
 // Runs the processing on that data then console.logs the result, if errors occurs it console.errors the errors
-transcribe(data)
-  .then(console.log)
-  .catch(console.error);
+transcribe(data).then(console.log).catch(console.error);
