5443f407918d0aa38beb1c75ce08c1725658b54f
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-05-19 17:45:13 -0400

    finished json_converter.py to search subfolders and convert all 314 .jpeg to .json containing base64

--- testing/json_converter.py
+++ testing/json_converter.py
@@ -1,39 +1,36 @@
 import base64
 import os
 
-path = "C:/Users/micha/Desktop/Lambda/DS-Unit-5_Labs/Story_Squad_Dataset/Transcribed_Stories/32--/"
+path = "C:/Users/micha/Desktop/Lambda/DS-Unit-5_Labs/Story_Squad_Dataset/Transcribed_Stories/"
 path_2 = "C:/Users/micha/Desktop/Lambda/DS-Unit-5_Labs/story-squad-ds/testing/json_files"
 
-for root, dirs, files in os.walk(path):
-    for name in files:
-        if name.endswith((".jpg")):
-            print(name)
-
+path_list = []
 
 for root, dirs, files in os.walk(path):
-    print("ROOT:", root)
-    print("DIRS:", dirs)
-    print("FILES:", files)
     for dir in dirs:
         input_path = os.path.join(path, dir)
         for root, dirs, files in os.walk(input_path):
-            for name in files:
-                if name.endswith((".jpg")):
-                    print("NAME:", name)
-                    input_path_2 = os.path.join(input_path, name)
-                    print("INPUT:", input_path_2)
+            for dir_2 in dirs:
+                input_path_2 = os.path.join(input_path, dir_2)
+                for root, dirs, files in os.walk(input_path_2):
+                    for name in files:
+                        if name.endswith((".jpg")):
+                            input_path_3 = os.path.join(input_path_2, name)
+                            path_list.append(input_path_3)
+                            
+                            with open(input_path_3, "rb") as img_file:
+                                my_string = base64.b64encode(img_file.read())
+                                my_string = my_string.decode()
 
-                    with open(input_path_2, "rb") as img_file:
-                        my_string = base64.b64encode(img_file.read())
-                        my_string = my_string.decode()
+                                file_name = os.path.splitext(name)[0].replace(" ", "_")
+                                json = f"{{\"images\": [\"data:image/jpg;base64,{my_string}\"], \"file_name\": \"{file_name}\"}}"
 
-                        file_name = os.path.splitext(name)[0].replace(" ", "_")
-                        json = f"{{\"images\": [\"data:image/jpg;base64,{my_string}\"], \"file_name\": \"{file_name}\"}}"
+                                file_name = os.path.join(path_2, file_name+".json")
+                                json_file = open(f"{file_name}", "w")
+                                json_file.write(json)
+                                json_file.close()
 
-                        file_name = os.path.join(path_2, file_name+".json")
-                        json_file = open(f"{file_name}", "w")
-                        json_file.write(json)
-                        json_file.close()
+print(len(path_list))
             
     # for name in files:
     #     if name.endswith((".jpg")):
