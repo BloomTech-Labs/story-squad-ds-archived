2c3b7b5dc6b8c67f279de8cab731ee14fe06506e
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-05-20 11:57:50 -0400

    docstring added to auto_google_vision.py and several files decremented to OBE

--- auto_google_vision.py
+++ auto_google_vision.py
@@ -1,6 +1,14 @@
 import os
 
+# Will need to change the path based on User's local environment
 path = "C:/Users/micha/Desktop/Lambda/DS-Unit-5_Labs/story-squad-ds/testing/json_files/"
+
+"""
+After images of handwritten text have been converted to JSON objects, this loops over each file and pipes
+it into the transcription.py file which sends it to the Google API . 
+
+When testing, remember to uncomment the path and block of code in the main() function in transcription.py 
+"""
 
 
 for image_path in os.listdir(path): 
@@ -9,19 +17,4 @@ for image_path in os.listdir(path):
     input_path = os.path.join(path, image_path)
     print(input_path)
     os.system(f"pipenv run python dotPy/transcription.py < {input_path}")
-    print("--------------------------------------------")
-    file_name = os.path.splitext(image_path)
-
-
-
-# # path = "C:/Users/micha/Desktop\Lambda\DS-Unit-5_Labs\Story_Squad_Dataset\Transcribed_Stories/31--"
-
-# name_list = []
-
-# for root, dirs, files in os.walk(path):
-#     for name in files:
-#         if name.endswith((".jpg")):
-#             print(name)
-#             name_list.append(name)
-
-# print(len(name_list))
+    print("--------------------------------------------")S
